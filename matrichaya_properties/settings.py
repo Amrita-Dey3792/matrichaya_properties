3370ba529669f43cc6f0d5a3203c06ce4b7d576d
Amrita-Dey3792 <amrita15-3792@diu.edu.bd>
2025-09-16 22:25:29 +0600

    add host header

--- matrichaya_properties/settings.py
+++ matrichaya_properties/settings.py
@@ -16,17 +16,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ofy$9cdo-re4dgkj&)53$wng3ppfibhkvi#!j2*uxi%*=e3nzd')
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
+ALLOWED_HOSTS = ['matrichaya-properties.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition    
